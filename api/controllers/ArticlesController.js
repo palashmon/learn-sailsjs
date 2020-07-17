51dcd5f2dfbbb8f75b9bae568bf4b90ad2489963
Palash Mondal <palashm@live.in>
2020-07-17 17:04:31 +0530

    Add the model CRUD logic

--- api/controllers/ArticlesController.js
+++ api/controllers/ArticlesController.js
@@ -6,7 +6,55 @@
  */
 
 module.exports = {
-  
+  list: async (req, res) => {
+    try {
+      const articles = await Articles.find({});
+      res.view("pages/list", { articles });
+    } catch (error) {
+      res.send(500, { error: "Database Error" });
+    }
+  },
+  add: (req, res) => {
+    res.view("pages/add");
+  },
+  create: async (req, res) => {
+    try {
+      const title = req.body.title;
+      const body = req.body.body;
+      await Articles.create({ title: title, body: body });
+      res.redirect("/articles/list");
+    } catch (error) {
+      res.send(500, { error: "Database Error" });
+    }
+  },
+  delete: async (req, res) => {
+    try {
+      await Articles.destroy({ id: req.params.id });
+      res.redirect("/articles/list");
+    } catch (error) {
+      res.send(500, { error: "Database Error" });
+    }
+  },
+  edit: async (req, res) => {
+    try {
+      const article = await Articles.findOne({ id: req.params.id });
+      res.view("pages/edit", { article });
+    } catch (error) {
+      res.send(500, { error: "Database Error" });
+    }
+  },
+  update: async (req, res) => {
+    try {
+      const title = req.body.title;
+      const body = req.body.body;
 
+      await Articles.update(
+        { id: req.params.id },
+        { title: title, body: body }
+      );
+      res.redirect("/articles/list");
+    } catch (error) {
+      res.send(500, { error: "Database Error" });
+    }
+  },
 };
-
--- config/routes.js
+++ config/routes.js
@@ -9,29 +9,31 @@
  */
 
 module.exports.routes = {
+  /***************************************************************************
+   *                                                                          *
+   * Make the view located at `views/homepage.ejs` your home page.            *
+   *                                                                          *
+   * (Alternatively, remove this and add an `index.html` file in your         *
+   * `assets` directory)                                                      *
+   *                                                                          *
+   ***************************************************************************/
+
+  "/": { view: "pages/homepage" },
+  "get /articles/list": "ArticlesController.list",
+  "get /articles/add": "ArticlesController.add",
+  "post /articles/create": "ArticlesController.create",
+  "post /articles/delete/:id": "ArticlesController.delete",
+  "get /articles/edit/:id": "ArticlesController.edit",
+  "post /articles/update/:id": "ArticlesController.update",
 
   /***************************************************************************
-  *                                                                          *
-  * Make the view located at `views/homepage.ejs` your home page.            *
-  *                                                                          *
-  * (Alternatively, remove this and add an `index.html` file in your         *
-  * `assets` directory)                                                      *
-  *                                                                          *
-  ***************************************************************************/
-
-  '/': { view: 'pages/homepage' },
-
-
-  /***************************************************************************
-  *                                                                          *
-  * More custom routes here...                                               *
-  * (See https://sailsjs.com/config/routes for examples.)                    *
-  *                                                                          *
-  * If a request to a URL doesn't match any of the routes in this file, it   *
-  * is matched against "shadow routes" (e.g. blueprint routes).  If it does  *
-  * not match any of those, it is matched against static assets.             *
-  *                                                                          *
-  ***************************************************************************/
-
-
+   *                                                                          *
+   * More custom routes here...                                               *
+   * (See https://sailsjs.com/config/routes for examples.)                    *
+   *                                                                          *
+   * If a request to a URL doesn't match any of the routes in this file, it   *
+   * is matched against "shadow routes" (e.g. blueprint routes).  If it does  *
+   * not match any of those, it is matched against static assets.             *
+   *                                                                          *
+   ***************************************************************************/
 };
